3e5ddac5fb1b6c98f9e9b15cc271c76da6bb1139
marcotf-git <marcotf.dev@gmail.com>
2018-05-24 15:55:46 -0300

    step 4

--- app/src/androidTest/java/com/udacity/gradle/builditbigger/EndpointsAsyncTaskTest.java
+++ app/src/androidTest/java/com/udacity/gradle/builditbigger/EndpointsAsyncTaskTest.java
@@ -5,11 +5,15 @@ import android.support.v4.util.Pair;
 import android.test.ApplicationTestCase;
 import android.app.Application;
 import android.text.TextUtils;
+import android.util.Log;
 
 import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
 
 
 public class EndpointsAsyncTaskTest extends ApplicationTestCase<Application> {
+
+    private static final String TAG = EndpointsAsyncTaskTest.class.getSimpleName();
 
     String mJokeString = null;
     Exception mError = null;
@@ -21,15 +25,18 @@ public class EndpointsAsyncTaskTest extends ApplicationTestCase<Application> {
 
     @Override
     protected void setUp() throws Exception {
+        // This sets the number of times countDown() must be invoked
         signal = new CountDownLatch(1);
     }
 
+    // This method is automatically called after the test completes
     @Override
     protected void tearDown() throws Exception {
+        // This will make the test pass the await
         signal.countDown();
     }
 
-    public void testEndspointAsyncTask() throws InterruptedException {
+    public void testEndspointsAsyncTask() throws InterruptedException {
 
         EndpointsAsyncTask task = new EndpointsAsyncTask();
         task.setListener(new EndpointsAsyncTask.EndpointsAsyncTaskListener() {
@@ -42,7 +49,10 @@ public class EndpointsAsyncTaskTest extends ApplicationTestCase<Application> {
         });
         task.execute(new Pair<Context, String>(getContext(), ""));
 
-        signal.await();
+        // It will wait the countDown or 2000ms time
+        signal.await(2000, TimeUnit.MILLISECONDS);
+
+        Log.v(TAG, "mJokeString:" + mJokeString);
 
         assertNull(mError);
         assertFalse(TextUtils.isEmpty(mJokeString));
--- javaJokes/src/main/java/com/example/android/javajokes/Joker.java
+++ javaJokes/src/main/java/com/example/android/javajokes/Joker.java
@@ -26,5 +26,6 @@ public class Joker {
         }
 
         return jokes[jokeCount];
+
     }
 }
